0d2689d542d553b13a0c9617e1ba07a393ef1385
CruszewskY <rtrmsqtfrncsciii@gmail.com>
2015-06-17 04:14:55 +0800

    fix all

--- app.js
+++ app.js
@@ -1,1 +1,3 @@
-$('img').attr('src','http://www.obscureathon.com/wp-content/uploads/2011/04/SexyUnicornGuy.jpg');
+setInterval(function(){
+	$('img').attr('src','http://www.obscureathon.com/wp-content/uploads/2011/04/SexyUnicornGuy.jpg');
+},500);
