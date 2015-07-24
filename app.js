16b93f4f0693cbb6206ef7dfbc0da14d2776ae04
Jasper John Cinatad <slytherin9090@gmail.com>
2015-07-24 01:16:44 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 setInterval(function(){
-	$('img').attr('src','http://www.obscureathon.com/wp-content/uploads/2011/04/SexyUnicornGuy.jpg');
+	$('img').attr('src','https://s-media-cache-ak0.pinimg.com/236x/01/9a/1f/019a1fcec1ff411b8c808355cb0e7953.jpg');
 },250);
